68bc7ff1ef8cd95d11f9586572e8ae1decaab2c8
Isaac Sosa <isaacsosadj@gmail.com>
2021-11-23 21:54:19 -0400

    Adding step 2 - exercise 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,9 +8,17 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
+    <div>
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
   )
 }
 
@@ -32,9 +40,7 @@ const App = () => {
   return (
     <div>
       <Header  courseName={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3 ]} />
       <Total   exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
